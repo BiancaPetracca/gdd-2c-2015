c88550689e3f417d60708bc9d233af196ab0186d
Bianca <biancapetracca@gmail.com>
2015-10-12 04:26:47 -0300

    adds: abm aeronave / rol

--- AerolineaFrba/AerolineaFrba/Abm Aeronave/Aeronave.Designer.cs
+++ AerolineaFrba/AerolineaFrba/Abm Aeronave/Aeronave.Designer.cs
@@ -28,11 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buscador = new System.Windows.Forms.TextBox();
+            this.agregar = new System.Windows.Forms.Button();
+            this.borrar = new System.Windows.Forms.Button();
+            this.modificar = new System.Windows.Forms.Button();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.combo = new System.Windows.Forms.ComboBox();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buscador
+            // 
+            this.buscador.Location = new System.Drawing.Point(22, 39);
+            this.buscador.Name = "buscador";
+            this.buscador.Size = new System.Drawing.Size(100, 20);
+            this.buscador.TabIndex = 0;
+            // 
+            // agregar
+            // 
+            this.agregar.Location = new System.Drawing.Point(410, 365);
+            this.agregar.Name = "agregar";
+            this.agregar.Size = new System.Drawing.Size(75, 23);
+            this.agregar.TabIndex = 2;
+            this.agregar.Text = "Agregar";
+            // 
+            // borrar
+            // 
+            this.borrar.Location = new System.Drawing.Point(287, 365);
+            this.borrar.Name = "borrar";
+            this.borrar.Size = new System.Drawing.Size(75, 23);
+            this.borrar.TabIndex = 1;
+            this.borrar.Text = "Borrar";
+            // 
+            // modificar
+            // 
+            this.modificar.Location = new System.Drawing.Point(536, 365);
+            this.modificar.Name = "modificar";
+            this.modificar.Size = new System.Drawing.Size(75, 23);
+            this.modificar.TabIndex = 3;
+            this.modificar.Text = "Modificar";
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.combo);
+            this.groupBox.Location = new System.Drawing.Point(12, 12);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(629, 414);
+            this.groupBox.TabIndex = 4;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Aeronaves";
+            // 
+            // combo
+            // 
+            this.combo.Location = new System.Drawing.Point(103, 114);
+            this.combo.Name = "combo";
+            this.combo.Size = new System.Drawing.Size(121, 21);
+            this.combo.TabIndex = 5;
+            // 
+            // Aeronave
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(653, 438);
+            this.Controls.Add(this.buscador);
+            this.Controls.Add(this.borrar);
+            this.Controls.Add(this.agregar);
+            this.Controls.Add(this.modificar);
+            this.Controls.Add(this.groupBox);
+            this.Name = "Aeronave";
+            this.Text = "Aeronave";
+            this.groupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridCell listadoAeronaves;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1;
+        private System.Windows.Forms.TextBox buscador;
+        private System.Windows.Forms.Button agregar; 
+        private System.Windows.Forms.Button borrar;
+        private System.Windows.Forms.Button modificar;
+        private System.Windows.Forms.ComboBox combo;
+
+
     }
 }
---
+++ AerolineaFrba/AerolineaFrba/Abm Rol/Listado.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace AerolineaFrba.Abm_Rol
+{
+    partial class Listado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox_SeleccionarRol = new System.Windows.Forms.GroupBox();
+            this.dataGridView_SeleccionRol = new System.Windows.Forms.DataGridView();
+            this.Indice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modificar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.button_Cerrar = new System.Windows.Forms.Button();
+            this.groupBox_SeleccionarRol.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_SeleccionRol)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox_SeleccionarRol
+            // 
+            this.groupBox_SeleccionarRol.Controls.Add(this.dataGridView_SeleccionRol);
+            this.groupBox_SeleccionarRol.Location = new System.Drawing.Point(21, 12);
+            this.groupBox_SeleccionarRol.Name = "groupBox_SeleccionarRol";
+            this.groupBox_SeleccionarRol.Size = new System.Drawing.Size(801, 408);
+            this.groupBox_SeleccionarRol.TabIndex = 21;
+            this.groupBox_SeleccionarRol.TabStop = false;
+            this.groupBox_SeleccionarRol.Text = "Seleccionar Rol";
+            // 
+            // dataGridView_SeleccionRol
+            // 
+            this.dataGridView_SeleccionRol.AllowUserToAddRows = false;
+            this.dataGridView_SeleccionRol.AllowUserToDeleteRows = false;
+            this.dataGridView_SeleccionRol.AllowUserToResizeColumns = false;
+            this.dataGridView_SeleccionRol.AllowUserToResizeRows = false;
+            this.dataGridView_SeleccionRol.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView_SeleccionRol.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView_SeleccionRol.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_SeleccionRol.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Indice,
+            this.Rol,
+            this.Modificar});
+            this.dataGridView_SeleccionRol.Location = new System.Drawing.Point(169, 53);
+            this.dataGridView_SeleccionRol.Name = "dataGridView_SeleccionRol";
+            this.dataGridView_SeleccionRol.Size = new System.Drawing.Size(472, 302);
+            this.dataGridView_SeleccionRol.TabIndex = 0;
+            // 
+            // Indice
+            // 
+            this.Indice.HeaderText = "Índice";
+            this.Indice.Name = "Indice";
+            this.Indice.Visible = false;
+            // 
+            // Rol
+            // 
+            this.Rol.HeaderText = "Rol";
+            this.Rol.Name = "Rol";
+            this.Rol.ReadOnly = true;
+            // 
+            // Modificar
+            // 
+            this.Modificar.HeaderText = "Modificar";
+            this.Modificar.Name = "Modificar";
+            this.Modificar.Text = "Seleccionar";
+            this.Modificar.UseColumnTextForButtonValue = true;
+            // 
+            // button_Cerrar
+            // 
+            this.button_Cerrar.Location = new System.Drawing.Point(747, 426);
+            this.button_Cerrar.Name = "button_Cerrar";
+            this.button_Cerrar.Size = new System.Drawing.Size(75, 23);
+            this.button_Cerrar.TabIndex = 1;
+            this.button_Cerrar.Text = "Cerrar";
+            this.button_Cerrar.UseVisualStyleBackColor = true;
+            // 
+            // Listado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(692, 451);
+            this.Controls.Add(this.button_Cerrar);
+            this.Controls.Add(this.groupBox_SeleccionarRol);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Listado";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Seleccion";
+            this.groupBox_SeleccionarRol.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_SeleccionRol)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_SeleccionarRol;
+        private System.Windows.Forms.DataGridView dataGridView_SeleccionRol;
+        private System.Windows.Forms.Button button_Cerrar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Indice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rol;
+        private System.Windows.Forms.DataGridViewButtonColumn Modificar;
+    }
+
+    }
